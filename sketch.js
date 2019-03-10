d1a1e582af3e735e1309327f4b7dbb007995d55e
KenzAndCJ <mackenzie.lewia@gmail.com>
2019-03-10 18:57:07 -0400

    Moon is now in the realtime spot

--- sketch.js
+++ sketch.js
@@ -1,59 +1,60 @@
-let stars = []; 
+let stars = [];
 let _moon;
-
-function setup() 
-{
+var hr;
+var isDay;
+function setup() {
 	createCanvas(windowWidth - 20, windowHeight - 40);
 	background('black');
 	textSize(60);
 
-	let r = random(100,150);
-	for (let i = 0; i < r; i++)
-	{
+	//is it less than 6 pm and greater than 6 am
+	isDay = hour() + 1 < 18 && hour() + 1 > 6;
+	hr = hour() % 12;
+	let r = random(100, 200);
+	for (let i = 0; i < r; i++) {
 		stars.push(new star());
 	}
 
-	_moon = new moon();
+	_moon = new Moon();
 }
 
 
-function draw()
-{	
+function draw() {
 	fill('white');
-	text('Kenz and CJ!', windowWidth/2 - 140, 60);
-	
-	for (let s of stars)
-	{
+	text('Kenz and CJ!', windowWidth / 2 - 200, 60);
+
+	for (let s of stars) {
 		s.display();
 	}
 
 	_moon.display();
 }
 
-function moon()
-{
-	this.img = loadImage('assets/moon.png');
-	this.posX = random(0, windowWidth);
-	this.posY = random(0, 60);
-	this.scale = 3;
-	this.display = function()
-	{
-		image(this.img, this.posX, this.posY, this.img.width * this.scale, this.img.height * this.scale);
+class Moon {
+	constructor() {
+		this.highest = 10;
+		this.img = loadImage('assets/moon.png');
+		this.posX = (windowWidth/12) * (hr - 6);
+		this.scale = 3;
+
+		this.display = function () {
+			image(this.img, 
+				this.posX - ( this.img.width * this.scale/2), 
+				(windowHeight - this.highest)/(12-hr) - ( this.img.width * this.scale/2), this.img.width * this.scale, this.img.height * this.scale);
+		}
 	}
 }
 
 
-function star()
-{
-	this.posX = random(0,windowWidth); 
-	this.posY = random(0,windowHeight);
-	this.size = random(2,8);
+function star() {
+	this.posX = random(0, windowWidth);
+	this.posY = random(0, windowHeight);
+	this.size = random(2, 8);
 
-	this.display = function()
-	{
+	this.display = function () {
 		ellipse(this.posX, this.posY, this.size)
 	}
 }
 
 
- 
+
